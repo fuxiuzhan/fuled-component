fafd85cd2f56007e8881d94ad1e8fb9c4a939fd2
admin <fuxiuzhan>
2021-12-05 15:39:59 +0800

    opts

--- fuled-name-starter/src/main/java/com/fxz/fuled/name/starter/NameAutoConfig.java
+++ fuled-name-starter/src/main/java/com/fxz/fuled/name/starter/NameAutoConfig.java
@@ -4,15 +4,28 @@ import com.alibaba.cloud.nacos.ConditionalOnNacosDiscoveryEnabled;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.alibaba.cloud.nacos.NacosServiceManager;
 import com.alibaba.cloud.nacos.discovery.NacosServiceDiscovery;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author fxz
+ */
+
+@Configuration(proxyBeanMethods = false)
 @ConditionalOnDiscoveryEnabled
 @ConditionalOnNacosDiscoveryEnabled
 public class NameAutoConfig {
 
     @Bean
+    @ConditionalOnMissingBean
+    public NacosDiscoveryProperties nacosProperties() {
+        return new NacosDiscoveryProperties();
+    }
+
+    @Bean
     public NacosServiceDiscovery nacosServiceDiscovery(NacosDiscoveryProperties discoveryProperties, NacosServiceManager nacosServiceManager) {
-        return new NacosServiceDiscovery(discoveryProperties, nacosServiceManager);
+        return new NacosServiceDiscoveryWrapper(discoveryProperties, nacosServiceManager);
     }
 }
