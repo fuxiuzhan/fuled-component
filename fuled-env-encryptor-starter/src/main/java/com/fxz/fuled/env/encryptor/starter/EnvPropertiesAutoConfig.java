d8930e14a2807ee8f30e8de9f02f54a0dc694cd4
admin <fuxiuzhan>
2021-12-11 00:03:06 +0800

    opts encrypt

--- fuled-env-encryptor-starter/src/main/java/com/fxz/fuled/env/encryptor/starter/EnvPropertiesAutoConfig.java
+++ fuled-env-encryptor-starter/src/main/java/com/fxz/fuled/env/encryptor/starter/EnvPropertiesAutoConfig.java
@@ -3,16 +3,21 @@ package com.fxz.fuled.env.encryptor.starter;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
-@Component
-public class EnvPropertiesAutoConfig  implements EnvironmentAware {
+/**
+ * @author fxz
+ */
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class EnvPropertiesAutoConfig implements EnvironmentAware {
     private ConfigurableEnvironment environment;
 
     @Bean
-    BeanFactoryPostProcessor testPro() {
+    @Order()
+    BeanFactoryPostProcessor envPropertiesProcessor() {
         return new EnvPropertiesProcessor(environment);
     }
 
