da9ff35be84b5f8968ce3a0b49227e16467bcc4a
admin <fuxiuzhan>
2022-02-03 14:00:04 +0800

    opts

---
+++ fuled-skywalking-satrter/src/main/java/com/fxz/component/fuled/skywalking/satrter/autoconfig/ComponentVersionConfiguration.java
@@ -0,0 +1,16 @@
+package com.fxz.component.fuled.skywalking.satrter.autoconfig;
+
+import com.fxz.fuled.common.version.ComponentVersion;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fxz
+ */
+@Configuration
+public class ComponentVersionConfiguration {
+    @Bean("SkywalkingComponentVersion")
+    public ComponentVersion configVersion() {
+        return new ComponentVersion("fuled-skywalking-component.version", "1.0.0.waterdrop", "fuled-skywalking-component");
+    }
+}
