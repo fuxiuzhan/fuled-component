f4fc6cb5ccb15032ede3d18f170a10f1af848183
fuxiuzhan <fuxiuzhan@163.com>
2022-07-12 23:51:54 +0800

    add empty modules

--- fuled-dynamic-threadpool-starter/src/main/java/com/fxz/fuled/dynamic/threadpool/starter/AutoConfiguration.java
+++ fuled-dynamic-threadpool-starter/src/main/java/com/fxz/fuled/dynamic/threadpool/starter/AutoConfiguration.java
@@ -4,6 +4,9 @@ package com.fxz.fuled.dynamic.threadpool.starter;
 import com.fxz.fuled.common.version.ComponentVersion;
 import com.fxz.fuled.dynamic.threadpool.ThreadPoolRegistry;
 import com.fxz.fuled.dynamic.threadpool.pojo.ThreadPoolProperties;
+import com.fxz.fuled.dynamic.threadpool.reporter.DefaultReporter;
+import com.fxz.fuled.dynamic.threadpool.reporter.Reporter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
@@ -18,5 +21,11 @@ public class AutoConfiguration {
     @Bean("dynamicThreadPoolVersion")
     public ComponentVersion configVersion() {
         return new ComponentVersion("fuled-dynamic-threadpool.version", "1.0.0.waterdrop", "fuled-dynamic-threadpool-component");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Reporter defaultReporter() {
+        return new DefaultReporter();
     }
 }
---
+++ fuled-sentinel-starter/src/main/java/com/fxz/fuled/sentinel/starter/AppApplication.java
@@ -0,0 +1,5 @@
+package com.fxz.fuled.sentinel.starter;
+
+public class AppApplication {
+    //TODO 增加sentinel自动配置
+}
