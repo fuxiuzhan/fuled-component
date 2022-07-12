8a6150a8345c795cd2bb1e382db6d51f72be7cf8
fuxiuzhan <fuxiuzhan@163.com>
2022-07-13 07:25:45 +0800

    add zuul gateway

---
+++ fuled-gateway-zuul-starter/src/main/java/com/fxz/fuled/gateway/zuul/starter/filter/AccessFilter.java
@@ -0,0 +1,41 @@
+package com.fxz.fuled.gateway.zuul.starter.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * 黑白名单限制
+ */
+
+@Component
+@Slf4j
+public class AccessFilter extends ZuulFilter implements InitializingBean {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
---
+++ fuled-gateway-zuul-starter/src/main/java/com/fxz/fuled/gateway/zuul/starter/locator/RouteLocator.java
@@ -0,0 +1,90 @@
+package com.fxz.fuled.gateway.zuul.starter.locator;
+
+import com.fxz.fuled.gateway.zuul.starter.properties.RoutesProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class RouteLocator extends SimpleRouteLocator implements RefreshableRouteLocator {
+
+    private ZuulProperties zuulProperties;
+
+    private RoutesProperties zuulRoutesProperties;
+
+    public RouteLocator(String servletPath, ZuulProperties properties) {
+        super(servletPath, properties);
+        this.zuulProperties = properties;
+        if (log.isDebugEnabled()) {
+            log.debug("servletPath:{}", servletPath);
+        }
+    }
+
+    public RouteLocator(String servletPath, ZuulProperties properties, RoutesProperties zuulRoutesProperties) {
+        super(servletPath, properties);
+        this.zuulProperties = properties;
+        this.zuulRoutesProperties = zuulRoutesProperties;
+        log.info("servletPath:{}", servletPath);
+    }
+
+    @Override
+    public void refresh() {
+        super.doRefresh();
+    }
+
+
+    public Map<String, ZuulRoute> getRoute() {
+        super.doRefresh();
+        return locateRoutes();
+    }
+
+    @Override
+    protected Map<String, ZuulRoute> locateRoutes() {
+        //从自定义配置中加载路由信息
+        Map<String, ZuulRoute> routesMap = locateRoutesFromProperties();
+
+        if (CollectionUtils.isEmpty(routesMap)) {
+            routesMap = super.locateRoutes();
+        }
+
+        //优化一下配置
+        LinkedHashMap<String, ZuulRoute> values = new LinkedHashMap<>();
+        for (Map.Entry<String, ZuulRoute> entry : routesMap.entrySet()) {
+            String path = entry.getKey();
+            // Prepend with slash if not already present.
+            if (!path.startsWith("/")) {
+                path = "/" + path;
+            }
+            if (StringUtils.hasText(this.zuulProperties.getPrefix())) {
+                path = this.zuulProperties.getPrefix() + path;
+                if (!path.startsWith("/")) {
+                    path = "/" + path;
+                }
+            }
+            values.put(path, entry.getValue());
+        }
+        return values;
+    }
+
+    protected Map<String, ZuulRoute> locateRoutesFromProperties() {
+        Map<String, ZuulRoute> routes = new LinkedHashMap<>();
+
+        List<ZuulRoute> zuulRouteList = zuulRoutesProperties.getZuulRoutes();
+        if (CollectionUtils.isEmpty(zuulRouteList)) {
+            return routes;
+        }
+        zuulRouteList.stream().filter(zuulRoute -> StringUtils.hasText(zuulRoute.getPath())).forEach(zuulRoute -> {
+            routes.put(zuulRoute.getPath(), zuulRoute);
+        });
+        return routes;
+    }
+
+}
