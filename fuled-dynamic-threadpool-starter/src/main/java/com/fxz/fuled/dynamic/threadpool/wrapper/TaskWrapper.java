0fed7a0d1cf1ceda1e2e9369ebe18d9c03e69e08
fuxiuzhan <fuxiuzhan@163.com>
2023-04-16 10:14:46 +0800

    opts callable

---
+++ fuled-dynamic-threadpool-starter/src/main/java/com/fxz/fuled/dynamic/threadpool/wrapper/TaskWrapper.java
@@ -0,0 +1,41 @@
+package com.fxz.fuled.dynamic.threadpool.wrapper;
+
+/**
+ * 类型标识
+ */
+public interface TaskWrapper {
+    /**
+     * 获取对应的线程池名称
+     *
+     * @return
+     */
+    default String getThreadPoolName() {
+        return "";
+    }
+
+    /**
+     * 队列等待时间
+     *
+     * @return
+     */
+    default long queuedDuration() {
+        return 0;
+    }
+
+    /**
+     * 执行时间
+     *
+     * @return
+     */
+    default long executedDuration() {
+        return 0;
+    }
+
+    /**
+     * 是否worker
+     * @return
+     */
+    default boolean isWorker() {
+        return Boolean.FALSE;
+    }
+}
