bf7008955c6abfd5f8d2cb3c3b2444ca46ca8340
fuled <fuxiuzhan@163.com>
2024-12-21 21:19:51 +0800

    add memo

--- fuled-gateway-zuul-starter/src/main/java/com/fxz/fuled/gateway/zuul/starter/auth/AuthService.java
+++ fuled-gateway-zuul-starter/src/main/java/com/fxz/fuled/gateway/zuul/starter/auth/AuthService.java
@@ -4,5 +4,5 @@ package com.fxz.fuled.gateway.zuul.starter.auth;
 import com.fxz.fuled.gateway.zuul.starter.pojo.JwtInfo;
 
 public interface AuthService {
-   JwtInfo getByToken( String token,String url);
+    JwtInfo getByToken(String token, String url);
 }
