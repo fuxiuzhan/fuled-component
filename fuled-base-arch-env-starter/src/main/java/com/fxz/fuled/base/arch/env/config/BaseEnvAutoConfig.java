dca0e874c003dde5933dd849259cb5e440ea0b43
fuled <fuxiuzhan@163.com>
2025-05-24 14:41:17 +0800

    add base env starter

--- fuled-base-arch-env-starter/src/main/java/com/fxz/fuled/base/arch/env/config/BaseEnvAutoConfig.java
+++ fuled-base-arch-env-starter/src/main/java/com/fxz/fuled/base/arch/env/config/BaseEnvAutoConfig.java
@@ -1,7 +1,9 @@
 package com.fxz.fuled.base.arch.env.config;
 
 import com.fxz.fuled.base.arch.env.locator.BaseEnvSourceLocator;
+import com.fxz.fuled.common.version.ComponentVersion;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -13,4 +15,10 @@ import org.springframework.context.annotation.Import;
 @ConditionalOnProperty(name = "spring.cloud.nacos.config.enabled", matchIfMissing = true)
 @Import(BaseEnvSourceLocator.class)
 public class BaseEnvAutoConfig {
+
+    @Bean("baseArchComponent")
+    public ComponentVersion configVersion() {
+        return new ComponentVersion("fuled-base-arch-env-starter.version", "1.1.0.waterdrop", "fuled-base-arch-env-starter");
+    }
+
 }
