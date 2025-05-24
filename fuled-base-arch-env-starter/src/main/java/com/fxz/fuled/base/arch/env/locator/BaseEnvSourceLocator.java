1abfc0cba8d06164aa99ea5c905d59d8d2b91512
fuled <fuxiuzhan@163.com>
2025-05-24 10:19:39 +0800

    add base env starter

--- fuled-base-arch-env-starter/src/main/java/com/fxz/fuled/base/arch/env/locator/BaseEnvSourceLocator.java
+++ fuled-base-arch-env-starter/src/main/java/com/fxz/fuled/base/arch/env/locator/BaseEnvSourceLocator.java
@@ -3,13 +3,21 @@ package com.fxz.fuled.base.arch.env.locator;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertySource;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class BaseEnvSourceLocator implements PropertySourceLocator {
+
+    private static final String ENV_NAME = "base_arch";
+
     @Override
     public PropertySource<?> locate(Environment environment) {
-        return null;
+        CompositePropertySource composite = new CompositePropertySource(
+                ENV_NAME);
+//        composite.addFirstPropertySource("base_data_id");
+//        composite.addFirstPropertySource("app_data_id");
+        return composite;
     }
 }
